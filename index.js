715c124324f37bbe181ae07449d9fc38f663c1bd
Leida_Gandy <leida.gandy@gmail.com>
2020-07-28 12:04:40 +0100

    convert actions to use constants, refactored dispatch calls

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // Library Code
 function createStore(reducer) {
-  // The store should have four parts
+  // The store should have four partss
   // 1. The state
   // 2. Get the state.
   // 3. Listen to changes on the state.
@@ -34,13 +34,53 @@ function createStore(reducer) {
 
 // App Code
 // Add todos reducer
+
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+
+function addTodoAction(todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  };
+}
+function removeTodoAction(id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  };
+}
+
+function toggleTodoAction(id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  };
+}
+
+function addGoalAction(goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  };
+}
+function removeGoalAction(id) {
+  return {
+    type: ADD_GOAL,
+    id,
+  };
+}
+
 function todos(state = [], action) {
   switch (action.type) {
-    case "ADD_TODO":
+    case ADD_TODO:
       return state.concat([action.todo]);
-    case "REMOVE_TODO":
+    case REMOVE_TODO:
       return state.filter((todo) => todo.id !== action.id);
-    case "TOGGLE_TODO":
+    case TOGGLE_TODO:
       return state.map((todo) =>
         todo.id !== action.id
           ? todo
@@ -54,9 +94,9 @@ function todos(state = [], action) {
 // Add goals reducer
 function goals(state = [], action) {
   switch (action.type) {
-    case "ADD_GOAL":
+    case ADD_GOAL:
       return state.concat([action.goal]);
-    case "REMOVE_GOAL":
+    case REMOVE_GOAL:
       return state.filter((goal) => goal.id !== action.id);
     default:
       return state;
@@ -77,63 +117,49 @@ store.subscribe(() => {
   console.log("The new state is: ", store.getState());
 });
 
-store.dispatch({
-  type: "ADD_TODO",
-  todo: {
+store.dispatch(
+  addTodoAction({
     id: 0,
     name: "Learn React Native",
     complete: false,
-  },
-});
-
-store.dispatch({
-  type: "ADD_TODO",
-  todo: {
+  })
+);
+store.dispatch(
+  addTodoAction({
     id: 1,
     name: "Build an App",
     complete: false,
-  },
-});
+  })
+);
 
-store.dispatch({
-  type: "ADD_TODO",
-  todo: {
+store.dispatch(
+  addTodoAction({
     id: 2,
     name: "Read a book",
     complete: true,
-  },
-});
+  })
+);
 
-store.dispatch({
-  type: "REMOVE_TODO",
-  id: 1,
-});
+store.dispatch(removeTodoAction(1));
 
-store.dispatch({
-  type: "TOGGLE_TODO",
-  id: 0,
-});
+store.dispatch(toggleTodoAction(0));
 
-store.dispatch({
-  type: "ADD_GOAL",
-  goal: {
+store.dispatch(
+  addGoalAction({
     id: 0,
     name: "Learn Redux",
-  },
-});
+  })
+);
 
-store.dispatch({
-  type: "ADD_GOAL",
-  goal: {
+store.dispatch(
+  addGoalAction({
     id: 1,
     name: "Build more React projects",
-  },
-});
+  })
+);
 
-store.dispatch({
-  type: "REMOVE_GOAL",
-  id: 0,
-});
+store.dispatch(removeGoalAction(0));
+
 //Now whenever we want to update the state of our store, all we need to do
 // is call the dispatch and pass it the action which occured.
 
