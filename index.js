4e0f996ebb5327ffc2bdc528dd0fffe09c619963
Leida_Gandy <leida.gandy@gmail.com>
2020-07-28 09:22:11 +0100

    handles remove and toggle actions

--- index.js
+++ index.js
@@ -1,60 +1,68 @@
 //This function will create us a brand new store
 
 // Library Code
-function createStore(reducer) {
+function createStore (reducer) {
   // The store should have four parts
   // 1. The state
   // 2. Get the state.
   // 3. Listen to changes on the state.
   // 4. Update the state
 
-  let state;
-  let listeners = [];
+  let state
+  let listeners = []
 
-  const getState = () => state;
+  const getState = () => state
 
   const subscribe = (listener) => {
-    listeners.push(listener);
+    listeners.push(listener)
     return () => {
-      listeners = listeners.filter((l) => l !== listener);
-    };
-  };
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
 
   const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach((listener) => listener());
-  };
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
 
   return {
     getState,
     subscribe,
     dispatch,
-  };
+  }
 }
 
 // App Code
-function todos(state = [], action) {
-  if (action.type === "ADD_TODO") {
-    return state.concat([action.todo]);
+function todos (state = [], action) {
+  switch(action.type) {
+    case 'ADD_TODO' :
+      return state.concat([action.todo])
+    case 'REMOVE_TODO' :
+      return state.filter((todo) => todo.id !== action.id)
+    case 'TOGGLE_TODO' :
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, { complete: !todo.complete }))
+    default :
+      return state
   }
-
-  return state;
 }
 
-const store = createStore(todos);
+const store = createStore(todos)
 
 store.subscribe(() => {
-  console.log("The new state is: ", store.getState());
-});
+  console.log('The new state is: ', store.getState())
+})
 
 store.dispatch({
-  type: "ADD_TODO",
+  type: 'ADD_TODO',
   todo: {
     id: 0,
-    name: "Learn Redux",
-    complete: false,
-  },
-});
+    name: 'Learn Redux',
+    complete: false
+  }
+})
 
 //Now whenever we want to update the state of our store, all we need to do
 // is call the dispatch and pass it the action which occured.
+
+//The app can now handle removing a todo item as well as toggling a todo.
