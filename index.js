1f37e721897a21cf5d337bb5ec87699d479ffc6f
Leida_Gandy <leida.gandy@gmail.com>
2020-08-05 10:16:53 +0100

    Build create store from scracth piece by piece, add comments to explain code

--- index.js
+++ index.js
@@ -1,216 +1,82 @@
-// ///This function will create us a brand new store
+// Generic Library Code: part of our state management library
+// This function is going to create us a brand new store.
 
-// function generateId() {
-//   return (
-//     Math.random().toString(36).substring(2) + new Date().getTime().toString(36)
-//   );
-// }
+function createStore(reducer) {
+  // The store should have 4 parts
+  // 1. get the state
+  // 2. listen for changes on state
+  // 3. update the state
 
-// // // App Code
+  let state; // this variable will hold the state of our entire application
+  let listeners = [];
 
-// const ADD_TODO = "ADD_TODO";
-// const REMOVE_TODO = "REMOVE_TODO";
-// const TOGGLE_TODO = "TOGGLE_TODO";
-// const ADD_GOAL = "ADD_GOAL";
-// const REMOVE_GOAL = "REMOVE_GOAL";
+  const getState = () => state;
 
-// function addTodoAction(todo) {
-//   return {
-//     type: ADD_TODO,
-//     todo,
-//   };
-// }
+  const subscribe = (listener) => {
+    listeners.push(listener); // we want to push into the listeners array the function that is passed to subscribe when it is invoked.
 
-// function removeTodoAction(id) {
-//   return {
-//     type: REMOVE_TODO,
-//     id,
-//   };
-// }
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+  
+  // dispatch will update/modify the state inside our actual store
 
-// function toggleTodoAction(id) {
-//   return {
-//     type: TOGGLE_TODO,
-//     id,
-//   };
-// }
+const dispatch = (action) => { // it needs to receive an action will tell dispacth the specific action that occured inside our application
+state = reducer(state, action) // state will be whatever gets returned from calling todos passing it the current state and the action that occured 
+// now we just modified the state, we need to loop trough the array of listeners so that any listener the user set up will be invoked
+listeners.forEach((listener ) => listener()) // for each listener inside the listeners array we just invoke it
+}
 
-// function addGoalAction(goal) {
-//   return {
-//     type: ADD_GOAL,
-//     goal,
-//   };
-// }
 
-// function removeGoalAction(id) {
-//   return {
-//     type: REMOVE_GOAL,
-//     id,
-//   };
-// }
+  return {
+    getState,
+    subscribe,
+    dispatch
+  };
+}
 
-// // Add todos reducer
-// function todos(state = [], action) {
-//   switch (action.type) {
-//     case ADD_TODO:
-//       return state.concat([action.todo]);
-//     case REMOVE_TODO:
-//       return state.filter((todo) => todo.id !== action.id);
-//     case TOGGLE_TODO:
-//       return state.map((todo) =>
-//         todo.id !== action.id
-//           ? todo
-//           : Object.assign({}, todo, { complete: !todo.complete })
-//       );
-//     default:
-//       return state;
-//   }
-// }
 
-// // Add goals reducer
+// App Code specific to our app - code that the user might write to decide how state should change
+// Reducer - it takes in the current state and an action and reduces that to a new state
+//The reducer function wil specify how the state will change based on the action which occured in the appication 
+function todos(state = [], action) { // if the state is undefined set it to an empty array 
+    // based on the action that occured it needs to return us the new copy of our state
+    // if statement will listen for a specific event type
+    if (action.type === "ADD_TODO") {
+      // how do we want to modify the state based on this "ADD_TODO" event?
+      // we want to take the new todo item which we can access from action.todo and concatenate that to our state
+      return state.concat([action.todo]); // concat will return us a new array and so we are not modifying the state directly
+    }
+    return state;
+  }
+   const store = createStore(todos) // createStore must be passed a reducer function when invoked, it returns a store object
 
-// function goals(state = [], action) {
-//   switch (action.type) {
-//     case ADD_GOAL:
-//       return state.concat([action.goal]);
-//     case REMOVE_GOAL:
-//       return state.filter((goal) => goal.id !== action.id);
-//     default:
-//       return state;
-//   }
-// }
+  // when we invoke create store we want the user to be able to pass in the specific reducer function that 
+  // is going to decide how the state should change based on the specific action that occured. 
 
-// const checker = (store) => (next) => (action) => {
-//   if (
-//     action.type === ADD_TODO &&
-//     action.todo.name.toLowerCase().includes("bitcoin")
-//   ) {
-//     return alert("Nope.That is a bad idea.");
-//   }
 
-//   if (
-//     action.type === ADD_GOAL &&
-//     action.goal.name.toLowerCase().includes("bitcoin")
-//   ) {
-//     return alert("Nope.That is a bad idea.");
-//   }
-//   return next(action);
-// };
+  // dispatch() is called with an Action
+  // the reducer that was passed to createStore() is called with the current state tree and the action.This updates the state tree. 
+  // Because the state has (potencially) changed, all listener functions that have been registered with the subscribe method are called)
+ 
+  //our store has 3 methods: getState, subscribe and dispatch
+//lets call store.subscribe, and whenever our state changes we want to console.log the new state by calling getState
 
-// const logger = (store) => (next) => (action) => {
-//   console.group(action.type);
-//   console.log("The action: ", action);
-//   const result = next(action);
-//   console.log("The new state: ", store.getState());
-//   console.groupEnd();
-//   return result;
-// };
+store.subscribe(() => {
+    console.log("The new state is: ", store.getState())
+})
 
-// // function checker(store) {
-// //   return function (next) {
-// //     return function (action) {
-// //       if (
-// //         action.type === ADD_TODO &&
-// //         action.todo.name.toLowerCase().includes("bitcoin")
-// //       ) {
-// //         return alert("Nope.That is a bad idea.");
-// //       }
+//next we want do dispatch an ACTION to see how the store will change
 
-// //       if (
-// //         action.type === ADD_GOAL &&
-// //         action.goal.name.toLowerCase().includes("bitcoin")
-// //       ) {
-// //         return alert("Nope.That is a bad idea.");
-// //       }
-// //       return next(action);
-// //     };
-// //   };
-// // }
+store.dispatch({     
+    type: "ADD_TODO",
+    todo: {
+        id: 0,
+        name: "Learn Redux",
+        complete: false
+    }
+})
 
-// const store = Redux.createStore(
-//   Redux.combineReducers({
-//     todos,
-//     goals,
-//   }),
-//   Redux.applyMiddleware(checker, logger)
-// );
-
-// store.subscribe(() => {
-//   const { goals, todos } = store.getState();
-
-//   document.getElementById("goals").innerHTML = "";
-//   document.getElementById("todos").innerHTML = "";
-
-//   goals.forEach(addGoalToDOM);
-//   todos.forEach(addTodoToDOM);
-// });
-
-// // DOM code
-// function addTodo() {
-//   const input = document.getElementById("todo");
-//   const name = input.value;
-//   input.value = "";
-
-//   store.dispatch(
-//     addTodoAction({
-//       name,
-//       complete: false,
-//       id: generateId(),
-//     })
-//   );
-// }
-
-// function addGoal() {
-//   const input = document.getElementById("goal");
-//   const name = input.value;
-//   input.value = "";
-
-//   store.dispatch(
-//     addGoalAction({
-//       id: generateId(),
-//       name,
-//     })
-//   );
-// }
-
-// document.getElementById("todoBtn").addEventListener("click", addTodo);
-
-// document.getElementById("goalBtn").addEventListener("click", addGoal);
-
-// function createRemoveButton(onClick) {
-//   const removeBtn = document.createElement("button");
-//   removeBtn.innerHTML = "X";
-//   removeBtn.addEventListener("click", onClick);
-//   return removeBtn;
-// }
-
-// function addTodoToDOM(todo) {
-//   const node = document.createElement("li");
-//   const text = document.createTextNode(todo.name);
-
-//   const removeBtn = createRemoveButton(() => {
-//     store.dispatch(removeTodoAction(todo.id));
-//   });
-
-//   node.appendChild(text);
-//   node.appendChild(removeBtn);
-//   node.style.textDecoration = todo.complete ? "line-through" : "none";
-//   node.addEventListener("click", () => {
-//     store.dispatch(toggleTodoAction(todo.id));
-//   });
-
-//   document.getElementById("todos").appendChild(node);
-// }
-
-// function addGoalToDOM(goal) {
-//   const node = document.createElement("li");
-//   const text = document.createTextNode(goal.name);
-//   const removeBtn = createRemoveButton(() => {
-//     store.dispatch(removeGoalAction(goal.id));
-//   });
-
-//   node.appendChild(text);
-//   node.appendChild(removeBtn);
-
-//   document.getElementById("goals").append(node);
-// }
+// when we execute store.dispatch, store.subscribe will run because we updated the state
+// we will see that there is a new object inside our state because that is what we specified in the reducer function
