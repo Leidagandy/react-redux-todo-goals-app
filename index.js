86cf8530fea5c26d336a0f67329f3920312b322e
Leida_Gandy <leida.gandy@gmail.com>
2020-08-05 14:07:12 +0100

    create action creators

--- index.js
+++ index.js
@@ -36,11 +36,47 @@ function createStore(reducer) {
   };
 }
 
-const ADD_TODO = "ADD_TODO"
-const REMOVE_TODO = "REMOVE_TODO"
-const TOGGLE_TODO = "TOGGLE_TODO"
-const ADD_GOAL = "ADD_GOAL"
-const REMOVE_GOAL = "REMOVE_GOAL"
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+
+//Action creators
+
+function addTodoAction(todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  };
+}
+
+function removeTodoAction(id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  };
+}
+function toggleTodoAction(id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  };
+}
+
+function addGoalAction(goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  };
+}
+function removeGoalAction(id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  };
+}
+
 // App Code specific to our app - code that the user might write to decide how state should change
 // Reducer - it takes in the current state and an action and reduces that to a new state
 //The reducer function wil specify how the state will change based on the action which occured in the appication
@@ -110,62 +146,49 @@ store.subscribe(() => {
 
 //next we want do dispatch an ACTION to see how the store will change
 
-store.dispatch({
-  type: ADD_TODO,
-  todo: {
+store.dispatch(
+  addTodoAction({
     id: 0,
     name: "Learn Redux",
     complete: false,
-  },
-});
-store.dispatch({
-    type: ADD_TODO,
-    todo: {
-      id: 1,
-      name: 'Wash the car',
-      complete: false,
-    }
   })
-  
-  store.dispatch({
-    type: ADD_TODO,
-    todo: {
-      id: 2,
-      name: 'Go to the gym',
-      complete: true,
-    }
+);
+
+store.dispatch(
+  addTodoAction({
+    id: 1,
+    name: "Wash the car",
+    complete: false,
   })
-  
-  store.dispatch({
-    type: REMOVE_TODO,
-    id: 1
+);
+
+store.dispatch(
+  addTodoAction({
+    id: 2,
+    name: "Go to the gym",
+    complete: true,
   })
-  
-  store.dispatch({
-    type: TOGGLE_TODO,
-    id: 0
+);
+
+store.dispatch(removeTodoAction(1));
+
+store.dispatch(toggleTodoAction(0));
+
+store.dispatch(
+  addGoalAction({
+    id: 0,
+    name: "Learn Redux",
   })
-  
-  store.dispatch({
-    type: ADD_GOAL,
-    goal: {
-      id: 0,
-      name: 'Learn Redux'
-    }
+);
+
+store.dispatch(
+  addGoalAction({
+    id: 1,
+    name: "Lose 20 pounds",
   })
-  
-  store.dispatch({
-    type: ADD_GOAL,
-    goal: {
-      id: 1,
-      name: 'Lose 20 pounds'
-    }
-  })
-  
-  store.dispatch({
-    type: REMOVE_GOAL,
-    id: 0
-  })
+);
+
+store.dispatch(removeGoalAction(0));
 
 // when we execute store.dispatch, store.subscribe will run because we updated the state
 // we will see that there is a new object inside our state because that is what we specified in the reducer function
